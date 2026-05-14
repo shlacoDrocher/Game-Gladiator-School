a9814b4983781eea8ddc26a20095ed5e6489deb5
Cheremsha <83032004amira@gmail.com>
2026-05-14 11:53:11 +0300

    Баланс: ослабить генерацию вражеских гладиаторов.

--- GladiatorFactory.cpp
+++ GladiatorFactory.cpp
@@ -16,8 +16,8 @@ static const std::vector<std::string> NAMES_LIST = {
 std::unique_ptr<Gladiator> GladiatorFactory::CreateRandomGladiator(int level) {
     size_t index = rand() % NAMES_LIST.size();
     std::string name = NAMES_LIST[index];
-    int randomHP = rand() % 50 + 51 + (level * 10);
-    int ramdomDamage = rand() % 11 + 5 + (level * 2);
+    int randomHP = rand() % 20 + 30 + (level * 2);
+    int ramdomDamage = rand() % 4 + 3 + (level * 1);
     int finalPrice = randomHP + (ramdomDamage * 2);
     return std::make_unique<Gladiator>(name, randomHP, ramdomDamage, finalPrice);
 }
